5412e1b3ae59aecb622b1bbdf73b2b7df306a998
pkabirr <pkabir.work@gmail.com>
2021-11-03 13:01:35 +0300

    Initial commit

---
+++ src/functions/functions.ts
@@ -0,0 +1,24 @@
+﻿import { v4 } from "uuid";
+
+/**
+ * Adds two numbers.
+ * @customfunction
+ * @param first First number
+ * @param second Second number
+ * @returns The sum of the two numbers.
+ */
+export function output(rowCount: number): string[][] {
+  const result: string[][] = [];
+
+  for (let i = 0; i < rowCount; ++i) {
+    let row: string[] = [];
+
+    for (let j = 0; j < 20; ++j) {
+      row.push(v4());
+    }
+
+    result.push(row);
+  }
+
+  return result;
+}
